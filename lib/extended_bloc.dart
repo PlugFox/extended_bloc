8a909e95cc074863a547d87bbc68d8051fa09bbc
Plague Fox <plugfox@gmail.com>
2021-05-30 01:30:58 +0300

    [#3] upd pipeline

--- lib/extended_bloc.dart
+++ lib/extended_bloc.dart
@@ -1,5 +1,7 @@
 library extended_bloc;
 
+export 'package:bloc/bloc.dart';
+
 export 'src/router/bloc_enum.dart';
 export 'src/router/bloc_router.dart';
 export 'src/salvation/bloc_visible_emit.dart';
